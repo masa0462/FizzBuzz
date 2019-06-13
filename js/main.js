f540b494f1cdde7f5e41401704861e75973d3254
masahiro <masahiro0462@gmail.com>
2019-06-13 22:50:35 +0900

    レビュー指摘対応

--- js/main.js
+++ js/main.js
@@ -1,46 +1,46 @@
 'use strict';
 
 {
-  const btn =document.getElementById('btn');
+  const btn = document.getElementById('btn');
 
-  btn.addEventListener('click',()=>{
+    btn.addEventListener('click',()=>{
     //変数の取得
-    var fizzLabel =document.getElementById('fizz');
-    var buzzLabel =document.getElementById('buzz');
-    var displayLabel =document.getElementById('display');
+    var fizzLabel = document.getElementById('fizz');
+    var buzzLabel = document.getElementById('buzz');
+    var displayLabel = document.getElementById('display');
     var fizzNum = fizzLabel.value;
     var buzzNum = buzzLabel.value;
 
     //初期化処理
-     if (displayLabel.hasChildNodes()){
-       for (var i =displayLabel.childNodes.length-1; i>=0; i--) {
-        displayLabel.removeChild(displayLabel.childNodes[i]);
-      }
-     }
+    displayLabel.textContent="";
+
 
 　　//メイン処理
     if(!(isNumber(fizzNum) && isNumber(buzzNum))){
       displayLabel.textContent = "整数を入力してください"
       return;
     }
+
     for(var i=1; i<=100; i++){
-      if(i%fizzNum===0 && i%buzzNum===0){
-        var liElement = document.createElement('li');
-        displayLabel.appendChild(liElement);
-        liElement.textContent=`fizzbuzz ${i}`;
-      }else if (i%fizzNum===0) {
-        var liElement = document.createElement('li');
-        displayLabel.appendChild(liElement);
-        liElement.textContent=`fizz ${i}`;
-      }else if (i%buzzNum===0) {
-        var liElement = document.createElement('li');
-        displayLabel.appendChild(liElement);
-        liElement.textContent=`buzz ${i}`;
+      if(i % fizzNum === 0 && i % buzzNum === 0){
+        var labelElement = document.createElement('label');
+        displayLabel.appendChild(labelElement);
+        labelElement.textContent = `fizzbuzz ${i}`;
+        labelElement.insertAdjacentHTML('afterend','<br>');
+      }else if (i % fizzNum === 0) {
+        var labelElement = document.createElement('label');
+        displayLabel.appendChild(labelElement);
+        labelElement.textContent = `fizz ${i}`;
+        labelElement.insertAdjacentHTML('afterend','<br>');
+      }else if (i % buzzNum === 0) {
+        var labelElement = document.createElement('label');
+        displayLabel.appendChild(labelElement);
+        labelElement.textContent = `buzz ${i}`;
+        labelElement.insertAdjacentHTML('afterend','<br>');
       }else{
-        var liElement = document.createElement('li');
-        displayLabel.appendChild(liElement);
-        liElement.textContent=i;
+        //何も処理しない。
       }
+
     }
   });
 
